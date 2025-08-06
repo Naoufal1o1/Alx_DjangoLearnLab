683146f3385822ce91069882f87f1d3ff609416f
Naoufal abdelkhalki <naoufal.abdelkhalki@gmail.com>
2025-08-06 11:58:42 +0100

    Fix Task 1: Re-add BookList view and path

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+# Task 1 view
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+# Task 2 viewset
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
