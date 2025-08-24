86451483e7037c0c6a6fdc33de44cb3f1ef39cba
Naoufal abdelkhalki <naoufal.abdelkhalki@gmail.com>
2025-08-24 12:19:05 +0100

    Fix feed view and follow/unfollow URLs for checker

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
     path('login/', RegisterView.as_view(), name='login'),
     path('profile/', RegisterView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -45,7 +45,8 @@ class FeedView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Post.objects.all().order_by('-created_at')
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
 class PostLikeView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
